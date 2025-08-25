7552e2a97eee3ab9fad4fbd8e0a43b992456e2d6
Shantanu Negi <shantanu_negi@hotmail.com>
2025-08-25 13:48:35 +1000

    Add project files.

---
+++ PasswordStrengthAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Your.Namespace
+{
+    public class PasswordStrengthAttribute : RegularExpressionAttribute
+    {
+        public int MinimumLength { get; set; } = 6;
+        public bool RequireUppercase { get; set; } = true;
+        public int MinNumberOfUppercase { get; set; } = 1;
+        public bool RequireLowercase { get; set; } = true;
+        public int MinNumberOfLowercase { get; set; } = 1;
+        public bool RequireDigit { get; set; } = true;
+        public int MinNumberOfDigit { get; set; } = 1;
+        public bool RequireSpecialCharacter { get; set; } = true; // any of @$!%*?&
+        public int MinNumberOfSpecialCharacter { get; set; } = 1;
+
+        public PasswordStrengthAttribute(int minimumLength = 6, bool requireUppercase = true, int minUpper = 1, 
+                                            bool requireLowercase = true, int minLower = 1, bool requireDigit = true, int minDigit = 1, bool requireSpecialCharacter = true, int minSpecialCharacter = 1)
+            : base(GetRegexPattern(minimumLength, requireUppercase, minUpper, requireLowercase, minLower, requireDigit, minDigit, requireSpecialCharacter, minSpecialCharacter))
+        {
+            MinimumLength = minimumLength;
+            RequireUppercase = requireUppercase;
+            RequireLowercase = requireLowercase;
+            RequireDigit = requireDigit;
+            RequireSpecialCharacter = requireSpecialCharacter;
+        }
+
+        private static string GetRegexPattern(int minLength, bool upper, int minUpper, bool lower, int minLower, bool digit, int minDigit, bool special, int minSpecialCharacter)
+        {
+            string pattern = "^";
+            if (upper)
+                pattern += "(?=(.*?[A-Z]){" + minUpper + ",})"; // min no of uppercase letter
+            if (lower)
+                pattern += "(?=(.*?[a-z]){" + minLower + ",})"; // min no of lowercase letter
+            if (digit)
+                pattern += "(?=(.*?\\d){" + minDigit + ",})"; // min no of digit
+            if (special)
+                pattern += "(?=(.*?[@$!%*?&]){" + minSpecialCharacter + ",})"; // min no of special character
+            pattern += $".{{{minLength},}}$"; // Minimum length
+            return pattern;
+        }
+        
+    }
+}
